59938f734407d711f62ed8c714d25beae5170c6b
CryptoThibault <thibault.chalaoux@gmail.com>
2021-03-13 18:55:46 +0100

    end

--- isMagicSquare.js
+++ isMagicSquare.js
@@ -1,3 +1,29 @@
+function isMagicSquare(tab) {
+  const carre = (x) => x * x
+  const reducer = (accumulator, currentValue) => accumulator + currentValue
+  const size = tab.length
+  var check = size * 2 + 2
+  var count = 0
+  var rottab = []; var d1tab = []; var d2tab = [];
+
+  for (k = 1; k - 1 < carre(size); k++) { count += k } //Count sum for each (row,col,diag)
+  count /= size
+
+  for (i = 0; i < size; i++) {
+    var xtab = []
+    for (j = 0; j < size; j++) { xtab.push(tab[j][i]) }
+    rottab.push(xtab)
+    d1tab.push(tab[i][i])
+    d2tab.push(tab[i][size - 1 - i])
+    if (tab[i].reduce(reducer) === count) { check-- } //Check row
+    if (rottab[i].reduce(reducer) === count) { check-- } //Check col
+  }
+  if (d1tab.reduce(reducer) === count) { check-- } //Check diag1
+  if (d2tab.reduce(reducer) === count) { check-- } //Check diag2
+
+  return check === 0 //true:false
+}
+
 const square1 = [
   [2, 7, 6],
   [9, 5, 1],
@@ -24,38 +50,6 @@ const square4 = [
   [25, 29, 10, 9, 26, 12],
   [36, 5, 33, 4, 2, 31],
 ]
-
-const carre = (x) => x * x
-const reducer = (accumulator, currentValue) => accumulator + currentValue
-
-function isMagicSquare(tab) {
-  const size = tab.length
-  var check = size * 2 + 2
-  var count = 0
-  var rottab = []; var d1tab = []; var d2tab = [];
-
-  for (i = 1; i - 1 < carre(size); i++) { count += i } //Count sum for each (row,col,diag)
-  count /= size
-
-  for (j = 0; j < size; j++) {
-    if (tab[j].reduce(reducer) === count) { check-- } //Check row
-    var xtab = []
-    for (k = 0; k < size; k++) {
-      xtab.push(tab[k][j])
-    }
-    rottab.push(xtab)
-    if (rottab[j].reduce(reducer) === count) { check-- } //Check col
-    d1tab.push(tab[j][j])
-    d2tab.push(tab[j][size - 1 - j])
-  }
-  if (d1tab.reduce(reducer) === count) { check-- } //Check diag1
-  if (d2tab.reduce(reducer) === count) { check-- } //Check diag2
-
-  return check === 0 //true:false
-}
-
-
-
 
 console.log(`The square 1 is ${isMagicSquare(square1) ? 'magic' : 'normal'}.`)
 console.log(`The square 2 is ${isMagicSquare(square2) ? 'magic' : 'normal'}.`)
